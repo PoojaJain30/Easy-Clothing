24bd9d5e104274afc66bd7433488d0d7f7c77f17
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-12-14 20:26:09 -0600

    added firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import "./sign-in.style.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
+import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -48,6 +50,7 @@ class SignIn extends React.Component {
           />
 
           <CustomButton type="submit"> Sign In </CustomButton>
+          <CustomButton onClick ={ signInWithGoogle }> Sign In With Google </CustomButton>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore' ;
+import 'firebase/auth';
+
+const config = {
+        apiKey: "AIzaSyDcrrzYXRve0t6CRAg32do6FwAPDv801tI",
+        authDomain: "easy-clothing-db.firebaseapp.com",
+        projectId: "easy-clothing-db",
+        storageBucket: "easy-clothing-db.appspot.com",
+        messagingSenderId: "914393020279",
+        appId: "1:914393020279:web:cb10ae4d2fc7f9ed81d372"
+    
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GithubAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
